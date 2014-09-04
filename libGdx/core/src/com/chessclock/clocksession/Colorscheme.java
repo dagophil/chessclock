b60198847aa62a30a16b343f07bdcb7d004f571d
Philip Schill <philip.schill@gmx.de>
2014-09-04 23:08:56 +0200

    Removed bgPressed from Colorscheme.

--- libGdx/core/src/com/chessclock/clocksession/Colorscheme.java
+++ libGdx/core/src/com/chessclock/clocksession/Colorscheme.java
@@ -2,14 +2,12 @@ package com.chessclock.clocksession;
 
 import com.badlogic.gdx.graphics.Color;
 
-// TODO: Is bgPressed needed?
 public class Colorscheme {
 	
 	public static Colorscheme getWhiteColorscheme() {
 		Colorscheme colors = new Colorscheme();
 		colors.bgActive = Color.WHITE;
 		colors.bgInactive = Color.valueOf("bbbbbb");
-		colors.bgPressed = Color.valueOf("dddddd");
 		return colors;
 	}
 	
@@ -17,12 +15,10 @@ public class Colorscheme {
 		Colorscheme colors = new Colorscheme();
 		colors.bgActive = Color.BLACK;
 		colors.bgInactive = Color.valueOf("222222");
-		colors.bgPressed = Color.valueOf("444444");
 		return colors;
 	}
 
 	public Color bgActive;
 	public Color bgInactive;
-	public Color bgPressed;
 	
 }
